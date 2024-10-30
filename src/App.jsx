73b73825c2b16b00926637f106dde11c4cddb8ea
anais <a.nais_60@live.fr>
2024-10-30 20:26:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Landing from "./pages/Landing";
+import Header from "./components/Header/Header";
+import "./assets/style/reset.scss";
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Landing />
+    </>
+  )
+}
+
+export default App

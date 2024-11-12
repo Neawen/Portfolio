c8332fa0d2b0b4aacaaaf8a5e8f8acf9e8f7152e
anais <a.nais_60@live.fr>
2024-11-12 12:50:42 +0100

    adding footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Landing from "./pages/Landing";
 import Header from "./components/Header/Header";
 import "./assets/style/main.scss";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   return (
     <>
       <Header />
       <Landing />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h2 className="footer__title">&copy; 2024 Lenoble Anaïs. Tous droits réservés.</h2>
+      <a
+        href="http://www.linkedin.com/in/anais-lenoble"
+        target="_blank"
+        className="footer__social"
+      >
+        <i className="fa-brands fa-linkedin"></i>
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;

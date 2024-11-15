e2bdecb3c3e5f324558dcbb8b5e24fdd9cda6ee1
anais <a.nais_60@live.fr>
2024-11-15 10:19:28 +0100

    removed compression plugin, simplified workflow for deployment on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// plugin to compress files
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({
-      algorithm: "brotliCompress",
-      threshold: 10240,
-    }),
   ],
 });

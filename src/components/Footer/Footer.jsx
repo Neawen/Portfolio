fed2d4ab5030e5ba9a37b24c0af598aa9da7de89
anais <a.nais_60@live.fr>
2024-11-20 23:46:44 +0100

    adding maximum size for large screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,16 +2,20 @@ import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <h2 className="footer__title">&copy; 2024 Lenoble Anaïs. Tous droits réservés.</h2>
-      <a
-        href="http://www.linkedin.com/in/anais-lenoble"
-        target="_blank"
-        className="footer__social"
-        aria-label="Lien vers Linkedin"
-      >
-        <i className="fa-brands fa-linkedin"></i>
-      </a>
+    <footer>
+      <div className="footer">
+        <h2 className="footer__title">
+          &copy; 2024 Lenoble Anaïs. Tous droits réservés.
+        </h2>
+        <a
+          href="http://www.linkedin.com/in/anais-lenoble"
+          target="_blank"
+          className="footer__social"
+          aria-label="Lien vers Linkedin"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
     </footer>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,41 +10,43 @@ const Header = () => {
 
   return (
     <header>
-      <div className="header">
-        <div className="logo">
-          <a href="#start">
-            <i className="fa-brands fa-pagelines logo__icon"></i>
-            <h2 className="logo__name">ANAIS</h2>
-          </a>
+      <div className="header-container">
+        <div className="header">
+          <div className="logo">
+            <a href="#start">
+              <i className="fa-brands fa-pagelines logo__icon"></i>
+              <h2 className="logo__name">ANAIS</h2>
+            </a>
+          </div>
+          <nav className="header__nav">
+            <a href="#about-section">À propos</a>
+            <a href="#projects-section">Projets</a>
+            <a href="#contact">Contact</a>
+          </nav>
+          <button
+            className="header__burger"
+            onClick={toggleSidebar}
+            aria-label="Ouvrir le menu d'options"
+          >
+            {activeSidebar ? (
+              // back
+              <i className="fa-solid fa-arrow-right"></i>
+            ) : (
+              // menu
+              <i className="fa-solid fa-bars"></i>
+            )}
+          </button>
         </div>
-        <nav className="header__nav">
-          <a href="#about-section">À propos</a>
-          <a href="#projects-section">Projets</a>
-          <a href="#contact">Contact</a>
-        </nav>
-        <button
-          className="header__burger"
-          onClick={toggleSidebar}
-          aria-label="Ouvrir le menu d'options"
-        >
-          {activeSidebar ? (
-            // back
-            <i className="fa-solid fa-arrow-right"></i>
-          ) : (
-            // menu
-            <i className="fa-solid fa-bars"></i>
-          )}
-        </button>
-      </div>
-      <div className={`sidebar ${activeSidebar && "sidebar--active"}`}>
-        <nav className="sidebar__nav">
-          <a href="#about-section">À propos</a>
-          <a href="#projects-section">Projets</a>
-          <a href="#contact">Contact</a>
-        </nav>
-        <div className="sidebar__config">
-          <p>Langage</p>
-          <p>Mode sombre</p>
+        <div className={`sidebar ${activeSidebar && "sidebar--active"}`}>
+          <nav className="sidebar__nav">
+            <a href="#about-section">À propos</a>
+            <a href="#projects-section">Projets</a>
+            <a href="#contact">Contact</a>
+          </nav>
+          <div className="sidebar__config">
+            <p>Langage</p>
+            <p>Mode sombre</p>
+          </div>
         </div>
       </div>
     </header>
